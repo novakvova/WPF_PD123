ee314cb7fb7ec68171cd13eff16d9001eb8982d1
novakvova <novakvova@gmail.com>
2022-06-15 22:52:10 +0300

    Show list data datagrid

--- WpfAppTest/ListUsersWindow.xaml.cs
+++ WpfAppTest/ListUsersWindow.xaml.cs
@@ -23,22 +23,30 @@ namespace WpfAppTest
     public partial class ListUsersWindow : Window
     {
         private ObservableCollection<UserVM> users = new ObservableCollection<UserVM>();
-
+        private MyDataContext context;
         public ListUsersWindow()
         {
             InitializeComponent();
-            users.Add(new UserVM()
+            context = new MyDataContext();
+            //users.Add(new UserVM()
+            //{
+            //    Id = 1,
+            //    Name="Сашко",
+            //    ImageUrl = "https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=1600&h=900"
+            //});
+            //users.Add(new UserVM()
+            //{
+            //    Id = 2,
+            //    Name = "Петро Васильович",
+            //    ImageUrl = @"C:\mydata\Study\2022\26. WPF PD123\Lesson 1. Begin\WpfAppTest\WpfAppTest\bin\Debug\net5.0-windows\images\150_lkfaxb4p.4uf.jpg"
+            //});
+            var data = context.Users.Select(x => new UserVM
             {
-                Id = 1,
-                Name="Сашко",
-                ImageUrl = "https://image.cnbcfm.com/api/v1/image/105992231-1561667465295gettyimages-521697453.jpeg?v=1561667497&w=1600&h=900"
-            });
-            users.Add(new UserVM()
-            {
-                Id = 2,
-                Name = "Петро Васильович",
-                ImageUrl = @"C:\mydata\Study\2022\26. WPF PD123\Lesson 1. Begin\WpfAppTest\WpfAppTest\bin\Debug\net5.0-windows\images\150_lkfaxb4p.4uf.jpg"
-            });
+                Id = x.Id,
+                Name = x.Name,
+                ImageUrl=x.Image
+            }).ToList();
+            users = new ObservableCollection<UserVM>(data);
             dgUsers.ItemsSource = users;
         }
 
@@ -59,6 +67,7 @@ namespace WpfAppTest
                 if (dgUsers.SelectedItem is UserVM)
                 {
                     var userView = (dgUsers.SelectedItem as UserVM);
+                    //userView.Id
                     userView.Name = "Катюха";
                     userView.ImageUrl = "https://kasta.ua/image/1035/uploads/product_image/2020/07/157/3a47b1be23f77bc5ad1d76137250bcc5.jpeg";
                 }
